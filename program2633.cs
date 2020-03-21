a70f2a54133cbddfd047390e5f6e7be4a7863c5d
erika-bit <erika.putri@students.amikom.ac.id>
2020-03-21 15:22:42 +0700

    Finished

---
+++ program2633.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    public class Taxi
+    {
+        public string DriverName;
+        public bool OnDuty;
+        public int NumPassenger;
+        public string Duty;
+
+        public void TaxiInfo()
+        {
+            if(OnDuty==true)
+            {
+                Duty = "Yes";
+            }
+            else
+            {
+                Duty = "No";
+            }
+            Console.WriteLine("Driver Name : " + DriverName);
+            Console.WriteLine("On Duty : " + Duty);
+            Console.WriteLine("Number of Passenger : " + NumPassenger + "\n");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(DriverName + " sedang menjemput penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(DriverName + " selesai mengantar penumpang");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Bima";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 4;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
